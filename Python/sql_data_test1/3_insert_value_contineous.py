c67aad05805ce3a63a7af73a2ae38b6b1f56aabd
JyunWei-Su <43307755+JyunWei-Su@users.noreply.github.com>
2022-07-11 23:23:58 +0800

    Update file.

--- Python/sql_data_test1/3_insert_value_contineous.py
+++ Python/sql_data_test1/3_insert_value_contineous.py
@@ -56,15 +56,29 @@ while(True):
         #anchor_type = obj['type']
         #unix_time = obj['unix_time']
         #uudf_time = obj['uudf_time']
-        print("********************************************")
-        sql = """INSERT INTO measurement
-                VALUES ('{anchor_type}',{unix_time},{uudf_time},
-                '01234567890a','987654321abc',-30,10,20,37, 'hello-world');"""\
-                .format(anchor_type = obj['type'], unix_time = obj['unix_time'], uudf_time = obj['uudf_time'])
+        sql = None
+        if(obj['type'] == 'rssi+aoa:xplr-aoa' and obj['data'] == 'measurement'):
+            sql = """INSERT INTO measurement (anchor_type, unix_time, uudf_time,
+                     instance_id, anchor_id, rssi, azimuth, elevation, channel)
+                     VALUES ('{anchor_type}',{unix_time},{uudf_time},
+                     '{instance_id}', '{anchor_id}', {rssi}, {azimuth}, {elevation}, {channel});"""\
+                     .format(anchor_type = obj['type'], unix_time = obj['unix_time'], uudf_time = obj['uudf_time'], \
+                             instance_id = obj['instance_id'], anchor_id = obj['anchor_id'], rssi = obj['rssi'], \
+                             azimuth = obj['azimuth'], elevation = obj['elevation'], channel = obj['channel'])
+            print(sql)
+            cur.execute(sql)
+        elif(obj['type'] == 'rssi:esp32' and obj['data'] == 'measurement'):
+            sql = """INSERT INTO measurement (anchor_type, unix_time, uudf_time,
+                     instance_id, anchor_id, rssi)
+                     VALUES ('{anchor_type}',{unix_time},{uudf_time},
+                     '{instance_id}', '{anchor_id}', {rssi});"""\
+                     .format(anchor_type = obj['type'], unix_time = obj['unix_time'], uudf_time = obj['uudf_time'], \
+                             instance_id = obj['instance_id'], anchor_id = obj['anchor_id'], rssi = obj['rssi'],)
+            print(sql)
+            cur.execute(sql)
         print("--------------------------------------------")
         #sql = sql.replace(':', '')
-        print(sql)
-        cur.execute(sql)
+
         # commit the changes to the database
         #conn.commit() # because conn haved set to autocommit, no longer to use this
                         # @see https://www.postgresqltutorial.com/postgresql-python/transaction/ 
